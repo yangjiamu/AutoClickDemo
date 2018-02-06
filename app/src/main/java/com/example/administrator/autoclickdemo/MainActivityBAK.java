6d3f1c40c7178c0434d87eb8ec83fd3899a17121
yangwenjie <646955803@qq.com>
2018-02-06 18:01:50 +0800

    init commit

---
+++ app/src/main/java/com/example/administrator/autoclickdemo/MainActivityBAK.java
@@ -0,0 +1,13 @@
+package com.example.administrator.autoclickdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityBAK extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_bak);
+    }
+}
